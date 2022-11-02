818f16c3a9d668dc4029d16c0bf246f20bc7a612
DevGibbs <chinexgibbs@gmail.com>
2022-11-02 16:04:31 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,14 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: string
+ * @s: string to be printed
  * Return: 0.
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s + 1)
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
